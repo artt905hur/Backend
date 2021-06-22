8b262ecf8846e2c2d6926b8cd54089ef497e841c
artt905hur <81934081+artt905hur@users.noreply.github.com>
2021-06-21 21:02:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -94,3 +94,93 @@ app.delete('/heaven/:id',
         res.send("Aí não! Guitarra quebrada.");
     }
 );
+
+/*
+  Daqui para baixo, uso o banco de dados MongoDB
+*/
+
+const mongodb = require('mongodb')
+
+
+const connectionString = `mongodb+srv://admin:uZGahLHXYfBRZD3@cluster0.fhdc2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const options = { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+};
+
+(async()=>{
+    const client = await mongodb.MongoClient.connect(connectionString, options);
+    const db = client.db('myFirstDatabase');
+    const heavens = db.collection('heavens');
+    console.log(await heavens.find({}).toArray());
+
+    app.get('/database',
+        async function(req, res){
+        // res.send(heaven);
+        res.send(await heavens.find({}).toArray());
+    }
+);
+
+app.get('/database/:id',
+    async function(req, res){
+        const id = req.params.id;
+        const heaven = await heavens.findOne(
+            {_id : mongodb.ObjectID(id)}
+        );
+        console.log(heaven);
+        if (!heaven){
+            res.send("Guitarra não encontrada");
+        } else {
+            res.send(heaven);
+        }
+    }
+);
+
+app.post('/database', 
+    async (req, res) => {
+        console.log(req.body);
+        const heaven = req.body;
+        
+        delete heaven["_id"];
+
+        heavens.insertOne(heaven);        
+        res.send("criar uma guitarra.");
+    }
+);
+
+app.put('/database/:id',
+    async (req, res) => {
+        const id = req.params.id;
+        const heaven = req.body;
+
+        console.log(heaven);
+
+        delete heaven["_id"];
+
+        const num_heavens = await heavens.countDocuments({_id : mongodb.ObjectID(id)});
+
+        if (num_heavens !== 1) {
+            res.send('Ocorreu um erro por conta do número de guitarras');
+            return;
+        }
+
+        await heavens.updateOne(
+            {_id : mongodb.ObjectID(id)},
+            {$set : heaven}
+        );
+        
+        res.send("Guitarra atualizada com sucesso.")
+    }
+)
+
+app.delete('/database/:id', 
+    async (req, res) => {
+        const id = req.params.id;
+        
+        await heavens.deleteOne({_id : mongodb.ObjectID(id)});
+
+        res.send("Guitarra removida com sucesso");
+    }
+);
+})();
